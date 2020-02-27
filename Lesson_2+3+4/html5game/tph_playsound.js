0efbe654c310f2821774f2d42d9fd71824f6ed8e
dangthevang <35418790+dangthevang@users.noreply.github.com>
2020-02-27 10:25:20 +0700

    10th

--- Lesson_2+3+4/html5game/tph_playsound.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("keyup", function() {
-    if(window.AudioContext && g_WebAudioContext && g_WebAudioContext.state == "suspended") {
-        g_WebAudioContext.resume();
-    }
- }, false);
